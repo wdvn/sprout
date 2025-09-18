89a670dbc98c48412b295ea4d154f50ef2046072
wdvn <nguyenthanhluynd@gmail.com>
2025-09-18 22:06:41 +0700

    update

--- src/wgvk/wgvk.hpp
+++ src/wgvk/wgvk.hpp
@@ -1,3 +1,4 @@
 #include "../../lib/wgvk/include/wgvk.h"
 #include "../../lib/wgvk/include/external/RGFW.h"
 //#include "../../lib/wgvk/include/external/incbin.h"
+#include "../../lib/wgvk/include/wgvk_structs_impl.h"
